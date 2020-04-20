1316fb0df8877e531b10b5887c4506fe1ff2b3a4
Luukuton <mleinikka@gmail.com>
2020-04-20 22:03:49 +0300

    Added automatic creation of the settings file if not present.

--- src/main/java/jirin/Main.java
+++ src/main/java/jirin/Main.java
@@ -1,9 +1,20 @@
 package jirin;
 
+import jirin.dao.Settings;
 import jirin.ui.JirinUI;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("settings.properties");
+
+        if (file.createNewFile()) {
+            Settings settings = new Settings("settings.properties");
+            settings.saveSettings("M Plus 1p", "M Plus 1p", "light");
+        }
+
         JirinUI.main(args);
     }
 }
