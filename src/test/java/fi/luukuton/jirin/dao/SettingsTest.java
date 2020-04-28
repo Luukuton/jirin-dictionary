c8972014262d97e4b7e37a4f14ef2ebf9fcaa29b
Luukuton <mleinikka@gmail.com>
2020-04-28 19:15:45 +0300

    Test for writing to settings file

--- src/test/java/fi/luukuton/jirin/dao/SettingsTest.java
+++ src/test/java/fi/luukuton/jirin/dao/SettingsTest.java
@@ -1,5 +1,6 @@
 package fi.luukuton.jirin.dao;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -8,12 +9,28 @@ import java.io.IOException;
 import static org.junit.Assert.*;
 
 public class SettingsTest {
-    @Test
-    public void equalWhenReadingSettingsFileIsSuccessful() throws IOException {
+
+    Settings settings;
+
+    @Before
+    public void setUp() throws IOException {
         File file = new File("src/test/resources/settingsTest.properties");
-        Settings settings = new Settings(file.getAbsolutePath());
+        settings = new Settings(file.getAbsolutePath());
+    }
+
+    @Test
+    public void equalWhenReadingSettingsFileIsSuccessful() {
         assertEquals("dark", settings.getTheme());
         assertEquals("M Plus 1p", settings.getSearchFont());
         assertEquals("M Plus 1p", settings.getContentFont());
+    }
+
+    @Test
+    public void equalWhenReadingAfterWritingSettingsFileIsSuccessful() throws IOException {
+        settings.saveSettings("M Plus 1p", "M Plus 1p", "light");
+        assertEquals("light", settings.getTheme());
+        assertEquals("M Plus 1p", settings.getSearchFont());
+        assertEquals("M Plus 1p", settings.getContentFont());
+        settings.saveSettings("M Plus 1p", "M Plus 1p", "dark");
     }
 }
